ab3bc771e8ac24181d8b17bf2056c7d4ebb7a93e
danbi <leedanbiii@naver.com>
2024-10-15 22:53:56 +0900

    Variables

---
+++ src/main/kotlin/com/example/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinplayground.basics
+
+fun main() {
+    // 상수
+    val name : String = "D"
+    println(name)
+//    name = "Dilip1"
+
+    // 변수
+    var age : Int = 33
+    println(age)
+    age = 34
+    println(age)
+}
