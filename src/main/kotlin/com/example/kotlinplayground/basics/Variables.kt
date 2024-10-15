4f1c10d06a143312c2c204b78fe962644e3c2774
danbi <leedanbiii@naver.com>
2024-10-15 23:17:52 +0900

    기본 데이터 타입, 간단한 문자열 처리

--- src/main/kotlin/com/example/kotlinplayground/basics/Variables.kt
+++ src/main/kotlin/com/example/kotlinplayground/basics/Variables.kt
@@ -2,13 +2,28 @@ package com.example.kotlinplayground.basics
 
 fun main() {
     // 상수
-    val name : String = "D"
+    val name = "D"
     println(name)
 //    name = "Dilip1"
 
     // 변수
-    var age : Int = 33
+    var age = 33
     println(age)
     age = 34
     println(age)
+
+    val salary = 30000L
+    println(salary)
+
+    val course = "Kotlin Spring"
+    println("course : $course and the course length is ${course.length}")
+
+    val multiline = "ABC \n DEF"
+    println(multiline)
+
+    val multiline1 = """
+        ABC
+        EDF
+    """.trimIndent()
+    println(multiline1)
 }
