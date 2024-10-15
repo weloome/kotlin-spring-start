e6816a3462bbe3263da9e56267e968b8e18537d2
danbi <leedanbiii@naver.com>
2024-10-15 23:36:28 +0900

    if else , when

---
+++ src/main/kotlin/com/example/kotlinplayground/basics/Conditions.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinplayground.basics
+
+fun main() {
+    // if-else
+    //when
+
+    val name = "Alex"
+//    val name = "danbi"
+    val result = if (name.length == 4) {
+        println("Name is Four Characters")
+        name
+    } else {
+        println("Name is not Four Characters")
+        name
+    }
+
+    println("result : $result")
+
+    var position = 1
+    position = 2
+//    val medal = if (position == 1) {
+//        "GOLD"
+//    } else if (position == 2) {
+//        "SILVER"
+//    } else if (position == 3) {
+//        "BRONZE"
+//    } else {
+//        "NO MEDAL"
+//    }
+
+    val medal = when (position) {
+        1 -> "GOLD"
+        2 -> "SILVER"
+        3 -> "BRONZE"
+        else -> "NO MEDAL"
+    }
+    println(medal)
+}
