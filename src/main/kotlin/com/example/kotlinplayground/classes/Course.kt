0528d9913a7330f6b3da72f3b8baef075b0990d4
leedanbi <leedanbiii@gentlemonster.com>
2024-10-17 15:21:06 +0900

    data class

---
+++ src/main/kotlin/com/example/kotlinplayground/classes/Course.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinplayground.classes
+
+data class Course(
+    val id: Int,
+    val name: String,
+    val author: String
+) {
+}
+
+fun main() {
+
+    val course = Course(1, "Reactive Programming in Modern Java using Project Reactor", "D")
+//    println(course)
+
+    val course1 = Course(2, "Reactive Programming in Modern Java using Project Reactor", "D")
+
+    // 동등 객체 비교
+    println("Checking Object Equality : ${course == course1}")
+
+    // 객체 복사
+    val course3 = course1.copy(
+        id=3, author = "D"
+    )
+    println(course3)
+}
