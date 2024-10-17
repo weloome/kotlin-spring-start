c8e09bc473ed1e1f3d51a72b267afda7a07ebd2b
leedanbi <leedanbiii@gentlemonster.com>
2024-10-17 13:57:52 +0900

    Constructors

--- src/main/kotlin/com/example/kotlinplayground/classes/Person.kt
+++ src/main/kotlin/com/example/kotlinplayground/classes/Person.kt
@@ -1,13 +1,20 @@
 package com.example.kotlinplayground.classes
 
-class Person {
-
+class Person(
+    //기본 생성자
+    val name : String = "",
+    val age : Int = 0
+) {
     fun action() {
         println("Person Walks")
     }
 }
 
 fun main() { // class를 실행할수 있게 해줌
-    val person = Person()
+    val person = Person("Danbi", 33)
     person.action()
+    println("Name : ${person.name} and the age is ${person.age}")
+
+    val person1 = Person()
+    println("Name : ${person1.name} and the age is ${person1.age}")
 }
