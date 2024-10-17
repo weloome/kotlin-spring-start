daa93de2a767bf04a7cef774cea62cd141e700d9
leedanbi <leedanbiii@gentlemonster.com>
2024-10-17 23:29:27 +0900

    null safety check

--- src/main/kotlin/com/example/kotlinplayground/nulls/ExploreNulls.kt
+++ src/main/kotlin/com/example/kotlinplayground/nulls/ExploreNulls.kt
@@ -1,13 +1,19 @@
 package com.example.kotlinplayground.nulls
 data class Movie(
     val id : Int?,
-    val name : String) {
+    val name : String)
 
+fun printName(name: String) {
+    println("Name is : $name")
 }
 
 fun main() {
     var nameNullable : String? = null
 
+//    printName(nameNullable!!)
+    nameNullable?.run {
+        printName(this)
+    }
     println("Value is : ${nameNullable?.length}") // safe operator , ?
     nameNullable = "Danbi"
     val length = nameNullable?.length?.toLong() ?: 0
@@ -30,6 +36,6 @@ fun main() {
 }
 
 fun saveMovie(movie: Movie): Movie {
-//    return movie.copy(id = 1)
-    return movie
+    return movie.copy(id = 1)
+//    return movie
 }
