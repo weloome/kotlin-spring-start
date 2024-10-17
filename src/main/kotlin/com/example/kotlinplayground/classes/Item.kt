7854564c70f80a9c06c3dcc0eac9c17b9421b671
leedanbi <leedanbiii@gentlemonster.com>
2024-10-17 15:52:22 +0900

    Getter & Setter

--- src/main/kotlin/com/example/kotlinplayground/classes/Item.kt
+++ src/main/kotlin/com/example/kotlinplayground/classes/Item.kt
@@ -2,6 +2,21 @@ package com.example.kotlinplayground.classes
 
 class Item() { // 빈 생성자
     var name : String = "" // name 속성 초기화
+    var price : Double = 0.0
+
+    get() {
+        println("Inside Getter")
+        // field: 프로퍼티의 실제 저장 공간
+        return field // price의 실제 값을 반환
+    }
+    set(value) {
+        println("Inside Setter")
+        if (value >= 0.0) {
+            field = value
+        } else {
+            throw IllegalArgumentException("Negative Price is not Allowed!")
+        }
+    }
 
     // constructor 키워드를 사용하여 보조 생성자 추가
     constructor(_name: String) : this() { // this()는 기본 생성자를 호출. 보조 생성자 실행 전 기본 생성자를 먼저 호출해야함
@@ -15,4 +30,7 @@ fun main() {
     println("Item name is ${item.name}")
     item.name = "Iphone 13"
     println("Item name is ${item.name}")
+
+    item.price = 10.0
+    println(item.price)
 }
