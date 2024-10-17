dc07966c5bb921192257eb140ea627fcea82c065
leedanbi <leedanbiii@gentlemonster.com>
2024-10-17 15:09:47 +0900

    init block

--- src/main/kotlin/com/example/kotlinplayground/classes/Person.kt
+++ src/main/kotlin/com/example/kotlinplayground/classes/Person.kt
@@ -6,6 +6,11 @@ class Person(
     val age : Int = 0
 ) {
     var email : String = ""
+    var nameLength: Int = 0
+    init { //초기화 블록
+        println("Inside Init Block")
+        nameLength = name.length
+    }
 
     constructor(
         _email : String,
@@ -20,13 +25,13 @@ class Person(
 }
 
 fun main() { // class를 실행할수 있게 해줌
-    val person = Person("Danbi", 33)
-    person.action()
-    println("Name : ${person.name} and the age is ${person.age}")
+//    val person = Person("Danbi", 33)
+//    person.action()
+//    println("Name : ${person.name} and the age is ${person.age}")
+//
+//    val person1 = Person()
+//    println("Name : ${person1.name} and the age is ${person1.age}")
 
-    val person1 = Person()
-    println("Name : ${person1.name} and the age is ${person1.age}")
-
-    val person2 = Person(_email = "aaa@gmail.com")
-    println("Name : ${person2.name} and the age is ${person2.age} and the email is ${person2.email}")
+    val person2 = Person(_email = "aaa@gmail.com", "LeeDanBi", 33)
+    println("Name : ${person2.name} and the age is ${person2.age} and the email is ${person2.email} and the nameLength is ${person2.nameLength}")
 }
