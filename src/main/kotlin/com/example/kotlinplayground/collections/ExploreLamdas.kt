9e053db24c02789d2adc9d9a9e86d784df0d53a6
leedanbi <leedanbiii@gentlemonster.com>
2024-10-19 20:05:05 +0900

    Higher-Order Functions

--- src/main/kotlin/com/example/kotlinplayground/collections/ExploreLamdas.kt
+++ src/main/kotlin/com/example/kotlinplayground/collections/ExploreLamdas.kt
@@ -1,5 +1,10 @@
 package com.example.kotlinplayground.collections
 
+// 고차원 함수 (함수안에 붙어있는 함수)
+fun calculate(x:Int, y:Int, op:(x:Int, y:Int) -> Int) : Int {
+    return op(x,y)
+}
+
 fun main() {
     val addLamda = { x: Int -> x+x }
     val addResult = addLamda(3)
@@ -11,4 +16,9 @@ fun main() {
     }
     val multiplyResult = multiplyLamda(2, 3)
     println("multiplyResult : $multiplyResult")
+
+    val result = calculate(2, 3, {a,b -> a*b})
+    val add = calculate(2, 3) {a,b -> a+b}
+    println("Result is $result")
+    println("Add is $add")
 }
