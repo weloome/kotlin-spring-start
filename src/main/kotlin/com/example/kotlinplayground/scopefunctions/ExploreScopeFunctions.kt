4b40c62ccba0fbd61ccf100abdd36a007c54ad7b
leedanbi <leedanbiii@gentlemonster.com>
2024-10-20 00:06:20 +0900

    let

--- src/main/kotlin/com/example/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
+++ src/main/kotlin/com/example/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
@@ -7,6 +7,23 @@ import com.example.kotlinplayground.classes.CourseCategory
 fun main() {
     exploreApply()
     exploreAlso()
+    exploreLet()
+}
+
+fun exploreLet() {
+    val numbers = mutableListOf(1,2,3,4,5)
+    val result = numbers.map { it*2 }.filter { it>5 }.let {
+        println(it)
+        it.sum()
+    }
+    println(result)
+
+    var name : String? = null
+    val result1 = name?.let {
+        println(it)
+        it.uppercase()
+    }
+    println(result1)
 }
 
 fun exploreAlso() {
